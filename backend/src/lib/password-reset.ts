c3b4e612bfbe7ea9575e35da1b31c87a39b6883f
Jaybro-git <janithmahanama47@gmail.com>
2026-04-14 02:28:26 +0530

    Waitlist Added

--- backend/src/lib/password-reset.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'node:crypto';
-import { env } from '../config/env.js';
-
-export const generatePasswordResetToken = () => crypto.randomBytes(32).toString('base64url');
-
-export const hashPasswordResetToken = (token: string) =>
-  crypto.createHmac('sha256', env.TOKEN_HMAC_SECRET).update(token).digest('hex');
